22fd14903bb84870fd2ea2112d4e110ba822469e
ChristianHAndrade <162520410+ChristianHAndrade@users.noreply.github.com>
2024-04-17 21:09:23 -0300

    banco de dados

---
+++ Amigo Secreto.sql
@@ -0,0 +1,3 @@
+INSERT INTO Grupo (nome_grupo, dt_inicio, dt_final,valor, tipo_amg_scrto, descricao) 
+VALUES ( "Empresa", "2024/04/17 ","2024/05/17", "100", "Presente", "husahus");
+SELECT * FROM GRUPO
